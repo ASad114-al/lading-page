42d6d52c9507ba406be137bbf9877c35eb173634
Asad114-al <as3ad.94aa@gmail.com>
2020-11-25 09:39:44 +0100

    new

---
+++ src/components/Firstsection.js
@@ -0,0 +1,45 @@
+
+const Bodysection = ( ) => {
+
+    return (
+        <section className="brand">
+      <div className="one">
+       <h3 style={{color: "green",textAlign: 'center' ,paddingTop :"2%"}}>What We Do</h3>
+       <p className="we">We`ve ot everything you need to launch and growy our businees</p>
+       </div>
+       <section className="Wedo">
+         <div>
+             
+            <span className="log"><img className="aos1" scr="./logo192.png" alt=""/>  Brand Identity</span>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+         </div>
+
+         <div>
+         
+            <span className="log"> <img className="aos1" scr="./logo192.png" alt=""/> Illustration</span>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+         </div>
+
+         <div>
+         
+            <span className="log"><img className="aos1" scr="./logo192.png" alt=""/>  Markiting</span>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+         </div>
+
+         <div>
+         
+            <span className="log"><img className="aos1" scr="./logo192.png" alt=""/>  Web Design</span>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.
+            Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat</p>
+         </div>
+      </section>
+
+      
+      </section>
+    )
+}
+
+export default Bodysection;
---
+++ src/components/head.js
@@ -0,0 +1,44 @@
+
+const HEADER = () =>
+{
+    
+    return ( 
+       
+        <header children ='header'>
+            <p id = 'titel'>Hello There</p>
+            <div id= 'h2border'>
+                <h2 >We Are Glint</h2>
+                </div>
+            <p id='titel-text'>Lorem ipsum dolor, sit amet consectetur adipisicing elit.  Fugiat nemo ipsum qui  cupiditate explicabo itaque vel corporis eum dolorum molestias hic, dolor similique voluptatem excepturi voluptates, dolorem at quibusdam saepe!</p>
+            <div className= 'number-header'>
+                <div id='number1-div' >
+                    <h1>127</h1>
+                    <p id = 'p'>Awrd Received</p>
+                </div>
+                <div id='number1-div'>
+                    <h1>1505</h1>
+                    <p id = 'p'> Cups of Coffee</p>
+                </div>
+                <div id='number1-div' >
+                    <h1>109</h1>
+                    <p id = 'p'>Projects Completed</p>
+                </div>
+                <div id='number'>
+                    <h1>102</h1>
+                    <p id = 'p'>Happy Clients</p>
+                </div>
+
+            </div>
+        </header>
+     
+    )
+    }
+    
+    export default HEADER;
+
+
+      
+      
+       
+      
+  
